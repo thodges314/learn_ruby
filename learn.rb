5450d57d5d7455b0264e0c1994efc324117ef4c3
Thomas Hodges <thodges@javascriptonline.org>
2017-01-23 11:56:55 -0800

    constants

--- learn.rb
+++ learn.rb
@@ -25,3 +25,19 @@ puts num_1.to_s + " - " + num_99.to_s + " = " + (num_1 - num_99).to_s
 
 big_float = 1.12345678901234
 puts(big_float + 0.00000000000005).to_s
+
+# objects
+
+puts 1.class
+puts 1.234.class
+puts "A String".class
+
+# # constants
+
+# A_CONSTANT = 1
+# A_CONSTANT = 2
+# puts A_CONSTANT.to_s
+
+# # learn.rb:38: warning: already initialized constant A_CONSTANT
+# # learn.rb:37: warning: previous definition of A_CONSTANT was here
+# # 2
