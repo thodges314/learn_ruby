fb18e05ae7758c6bdbc9e9cc1cf816f7279bf0f1
Thomas Hodges <thodges@javascriptonline.org>
2017-01-23 11:46:19 -0800

    float accuracy

--- learn.rb
+++ learn.rb
@@ -1,17 +1,27 @@
-# comments begin with an octhothorpe
+# # comments begin with an octhothorpe
 
-print "Enter a value: "
+# print "Enter a value: "
 
-first_num = gets.to_i
+# first_num = gets.to_i
 
-print "Enter another value: "
+# print "Enter another value: "
 
-second_num = gets.to_i
+# second_num = gets.to_i
 
-# arithmetic operators
+# # arithmetic operators
 
-puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
-puts first_num.to_s + " - " + second_num.to_s + " = " + (first_num - second_num).to_s
-puts first_num.to_s + " * " + second_num.to_s + " = " + (first_num * second_num).to_s
-puts first_num.to_s + " / " + second_num.to_s + " = " + (first_num / second_num).to_s
-puts first_num.to_s + " % " + second_num.to_s + " = " + (first_num % second_num).to_s
+# puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+# puts first_num.to_s + " - " + second_num.to_s + " = " + (first_num - second_num).to_s
+# puts first_num.to_s + " * " + second_num.to_s + " = " + (first_num * second_num).to_s
+# puts first_num.to_s + " / " + second_num.to_s + " = " + (first_num / second_num).to_s
+# puts first_num.to_s + " % " + second_num.to_s + " = " + (first_num % second_num).to_s
+
+# accuracy
+
+num_1 = 1.000
+num_99 = 0.999
+
+puts num_1.to_s + " - " + num_99.to_s + " = " + (num_1 - num_99).to_s
+
+big_float = 1.12345678901234
+puts(big_float + 0.00000000000005).to_s
