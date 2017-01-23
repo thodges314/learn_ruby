5049e71b39544b21e6ab61e2f9961a54badcd131
Thomas Hodges <thodges@javascriptonline.org>
2017-01-23 11:36:49 -0800

    basic arithmetic

---
+++ learn.rb
@@ -0,0 +1,17 @@
+# comments begin with an octhothorpe
+
+print "Enter a value: "
+
+first_num = gets.to_i
+
+print "Enter another value: "
+
+second_num = gets.to_i
+
+# arithmetic operators
+
+puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+puts first_num.to_s + " - " + second_num.to_s + " = " + (first_num - second_num).to_s
+puts first_num.to_s + " * " + second_num.to_s + " = " + (first_num * second_num).to_s
+puts first_num.to_s + " / " + second_num.to_s + " = " + (first_num / second_num).to_s
+puts first_num.to_s + " % " + second_num.to_s + " = " + (first_num % second_num).to_s
